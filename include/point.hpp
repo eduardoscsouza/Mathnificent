52ba6d717811df312234fb5e7d584b86af7bd331
wqferr <wqferr@gmail.com>
2017-04-18 12:59:27 -0300

    Add z coord to Point

--- include/point.hpp
+++ include/point.hpp
@@ -7,9 +7,10 @@ class Point
     public:
         const float x;
         const float y;
+        const float z;
 
-        Point(float x, float y) : x(x), y(y) {}
-        Point(const Point& p) : x(p.x), y(p.y) {}
+        Point(float x, float y, float z=0) : x(x), y(y), z(z) {}
+        Point(const Point& p) : x(p.x), y(p.y), z(p.z) {}
 };
 
 #endif
