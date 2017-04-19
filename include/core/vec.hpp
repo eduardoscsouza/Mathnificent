50519a6f058f4df86103aebd4b34ca6b45bd5ea4
wqferr <wqferr@gmail.com>
2017-04-19 17:42:10 -0300

    Add more operations to `Vector'

--- include/core/vec.hpp
+++ include/core/vec.hpp
@@ -2,6 +2,8 @@
 #ifndef POINT_HPP
 #define POINT_HPP 1
 
+#include <cmath>
+
 class Vector
 {
     public:
@@ -10,12 +12,103 @@ class Vector
         float z;
 
         Vector(float x, float y, float z=0) : x(x), y(y), z(z) {}
-        Vector(const Vector& p) : x(p.x), y(p.y), z(p.z) {}
+        Vector(const Vector& v) : x(v.x), y(v.y), z(v.z) {}
 
-        const Vector operator+(const Vector& p) const
+        float norm(void) const
         {
-            return Vector(this->x + p.x, this->y + p.y, this->z + p.z);
+            return std::sqrt(this->norm2());
         }
+
+        float norm2(void) const
+        {
+            return this->x*this->x + this->y*this->y + this->z*this->z;
+        }
+
+        float dist(const Vector& v) const
+        {
+            return (*this - v).norm();
+        }
+
+
+        Vector add(const Vector& v) const
+        {
+            return Vector(this->x+v.x, this->y+v.y, this->z+v.z);
+        }
+
+        Vector sub(const Vector& v) const
+        {
+            return Vector(this->x-v.x, this->y-v.y, this->z-v.z);
+        }
+
+        Vector opposite(void) const
+        {
+            return Vector(-this->x, -this->y, -this->z);
+        }
+
+        Vector scale(float f) const
+        {
+            return Vector(this->x*f, this->y*f, this->z*f);
+        }
+
+        float dot(const Vector& v) const
+        {
+            return this->x*v.x + this->y*v.y + this->z*v.z;
+        }
+
+        Vector cross(const Vector& v) const
+        {
+            return Vector
+            (
+                this->y*v.z - this->z*v.y,
+                this->z*v.x - this->x*v.z,
+                this->x*v.y - this->y*v.x
+            );
+        }
+
+
+        const Vector operator+(const Vector& v) const
+        {
+            return this->add(v);
+        }
+
+
+        const Vector operator-(const Vector& v) const
+        {
+            return this->sub(v);
+        }
+
+        const Vector operator-(void) const
+        {
+            return this->opposite();
+        }
+
+
+        const Vector operator*(float f) const
+        {
+            return this->scale(f);
+        }
+
+        friend const Vector operator*(float f, const Vector& v)
+        {
+            return v.scale(f);
+        }
+
+        float operator*(const Vector& v) const
+        {
+            return this->dot(v);
+        }
+
+        const Vector operator^(const Vector& v) const
+        {
+            return this->cross(v);
+        }
+
+
+        const Vector operator/(float f) const
+        {
+            return this->scale(1/f);
+        }
+
 
         Vector& operator+=(const Vector& p)
         {
@@ -24,11 +117,6 @@ class Vector
             this->z += p.z;
 
             return *this;
-        }
-
-        const Vector operator-(const Vector& p) const
-        {
-            return Vector(this->x - p.x, this->y - p.y, this->z - p.z);
         }
 
         Vector& operator-=(const Vector& p)
@@ -40,40 +128,34 @@ class Vector
             return *this;
         }
 
-        const Vector operator-(void) const
+        Vector& operator*=(float f)
         {
-            return Vector(-this->x, -this->y, -this->z);
+            this->x *= f;
+            this->y *= f;
+            this->z *= f;
+
+            return *this;
+        }
+        
+        Vector& operator^=(const Vector& v)
+        {
+            Vector n = this->cross(v);
+            this->x = n.x;
+            this->y = n.y;
+            this->z = n.z;
+
+            return *this;
         }
 
-        const Vector operator*(float f) const
+        Vector& operator/=(float f)
         {
-            return Vector(this->x * f, this->y * f, this->z * f);
+            this->x /= f;
+            this->y /= f;
+            this->z /= f;
+
+            return *this;
         }
 
-        friend const Vector operator*(float f, const Vector& v)
-        {
-            return Vector(v.x * f, v.y * f, v.z * f);
-        }
-
-        float operator*(const Vector& v)
-        {
-            return this->x*v.x + this->y*v.y + this->z*v.z;
-        }
-
-        const Vector operator/(float f)
-        {
-            return Vector(this->x / f, this->y / f, this->z / f);
-        }
-
-        const Vector operator^(const Vector& v)
-        {
-            return Vector
-            (
-                this->y*v.z - this->z*v.y,
-                this->z*v.x - this->x*v.z,
-                this->x*v.y - this->y*v.x
-            );
-        }
 
         bool operator==(const Vector& p) const
         {
